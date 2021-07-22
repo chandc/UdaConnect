a513c9f0489d248df248daf5508ab4bd4f7e45ec
chandc <chandc999@gmail.com>
2021-07-22 17:03:51 -0400

    implemented

---
+++ consumer.py
@@ -0,0 +1,16 @@
+from kafka import KafkaConsumer
+from json import loads
+
+# https://towardsdatascience.com/kafka-python-explained-in-10-lines-of-code-800e3e07dad1
+
+consumer = KafkaConsumer(
+    'numtest',
+     bootstrap_servers=['localhost:9092'],
+     auto_offset_reset='earliest',
+     enable_auto_commit=True,
+     group_id='my-group',
+     value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+for message in consumer:
+    message = message.value
+    print('{} read'.format(message)     )
